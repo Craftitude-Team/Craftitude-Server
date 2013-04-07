5a1b6ec57cddaccce7ff615c589c7099eca90e19
Icedream <icedream2k9@die-optimisten.net>
2013-04-07 22:58:09 +0200

    Almost empty LocalRepository class.

--- LocalRepository.cs
+++ LocalRepository.cs
@@ -2,10 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace Craftitude.Server
 {
     public class LocalRepository
     {
+        public DirectoryInfo RepositoryDirectory;
+
+        public LocalRepository(DirectoryInfo repositoryDir)
+        {
+            RepositoryDirectory = repositoryDir;
+        }
+
+        public Package GetPackage(string packageName)
+        {
+        }
+
+        public Package[] GetPackages()
+        {
+        }
     }
 }
